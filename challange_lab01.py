185a8626d5f6d10123661af4157e8d48096f5db3
SimonChung <simon84@gmail.com>
2020-05-04 14:26:54 -0400

    Day 1 update

---
+++ challange_lab01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+name = input("What is your  name? ")
+day = input("What is the day of the week? ")
+print ("Hello  " + name + "! " + "Happy " + day + "!")
+
+# F String
+print(f"Hello, {name}! Happy {day}!")
